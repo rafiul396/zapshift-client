41344277ba368bdea75387b7f9ce39e359c701f7
rafiul from office <rafiulofficial396@gmail.com>
2025-11-23 18:33:56 +0600

    updated my parcel page

--- src/root/dashborad/MyParcel.jsx
+++ src/root/dashborad/MyParcel.jsx
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { FaEdit } from 'react-icons/fa';
+import { MdOutlineDeleteOutline } from 'react-icons/md';
+import Swal from 'sweetalert2';
 
 const MyParcel = () => {
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['myParcels'],
         queryFn: async () => {
             const res = await axiosSecure.get('/parcels')
@@ -13,9 +16,70 @@ const MyParcel = () => {
         }
     })
 
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`parcels/${id}`)
+                .then(res => {
+                    refetch();
+                    console.log(res.data);
+                })
+                
+
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+            }
+        });
+    }
+
     return (
         <div>
             My Parcels {parcels.length}
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Serial No</th>
+                            <th>Name</th>
+                            <th>Products</th>
+                            <th>Description</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            parcels.map((parcel, index) => <tr key={parcel._id}>
+                                <th>{index + 1}</th>
+                                <td>{parcel.receivers_Name}</td>
+                                <td>{parcel.parcel_Name}</td>
+                                <td>{parcel.delivery_Instruction}</td>
+                                <td className='space-x-2'>
+                                    <button className="btn btn-soft">
+                                        <FaEdit />
+                                    </button>
+                                    <button onClick={() => handleDelete(parcel._id)} className="btn btn-soft">
+                                        <MdOutlineDeleteOutline />
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
